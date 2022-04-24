d2d9bd600f3ad43ad60242c2916fa3befc7c9423
dimatelichin999 <101800959+dimatelichin999@users.noreply.github.com>
2022-04-24 21:12:03 +0300

    нерівність

--- less5.java
+++ less5.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class less5 {
 
-    public static 
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        getResult(a,b,c);
+    }
+    public static void getResult(double a , double b, double c){
+
+        if (a < b){
+            if (b < c){
+                System.out.println("рівність є правильною");
+            }
+            else {
+                System.out.println("рівність не є правильною");
+            }
+        }
+        else {
+            System.out.println("рівність не є правильною");
+        }
+    }
 }
